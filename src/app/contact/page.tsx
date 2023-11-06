01ca1934e642800791456a0b095d89df19755b2b
Maroprecious <maroumukoro98@gmail.com>
2023-11-06 10:23:37 +0100

    fixes

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -14,12 +14,12 @@ import { Layout } from "../../../public/components/layout";
 const Modal = () => {
   return (
     <Layout>
-      <div className="bg-white w-full h-fit py-10 lg:py-0 lg:h-[100vh] text-black">
+      <div className="bg-white w-full h-full py-10 pb-[10rem] lg:py-0 lg:h-[100vh] text-black">
         <div className="flex justify-end items-end w-full p-6">
         </div>
         <div className="flex justify-center items-center w-full">
           <div className="flex justify-between flex-col lg:flex-row items-center w-[90%] lg:max-w-[1400px]">
-            <div className="flex justify-start mb-6 lg:mb-0 w-full lg:w-[47%] items-center gap-4">
+            <div className="flex justify-start mb-6 lg:mb-0 w-full lg:w-[45%] items-center gap-4">
               <div className="h-[40px] transition ease-in-out cursor-pointer duration-500 flex justify-center items-center w-[40px] border border-black rounded-[50%]">
                 <img src="/assets/insta.png" className="w-[30px] h-[30px] object-center object-contain" />
               </div>
@@ -27,7 +27,7 @@ const Modal = () => {
                 <AiFillFacebook size={25} fill="#3b5998" />
               </div>
             </div>
-            <div className="flex flex-col lg:flex-row w-full lg:w-[47%] justify-between items-start gap-6 lg:gap-0 lg:items-center">
+            <div className="flex flex-col lg:flex-row w-full lg:w-[50%] justify-between items-start gap-6 lg:gap-0 lg:items-center">
               <div>
                 <p className="font-bold text-lg">My Property Manager</p>
                 <p className="font-regular text-sm">28 Shakiru Anjorin Street, Off Admiralty<br/> Way, Lekki Phase One, Lagos, Nigeria</p>
@@ -41,7 +41,7 @@ const Modal = () => {
         </div>
         <div className="w-full flex justify-center items-center">
           <div className="w-[90%] flex-col lg:flex-row flex justify-between items-start">
-            <div className="w-full lg:w-[47%]">
+            <div className="w-full lg:w-[45%]">
               <p className={` mt-6 font-bold text-[35px]`}>
                 Get in Touch
               </p>
@@ -70,7 +70,7 @@ const Modal = () => {
                 <button className="w-full lg:w-[300px] bg-blue text-[#FFF] p-[1rem]">Contact Us</button>
               </div>
             </div>
-            <div className={`w-full lg:w-[47%]`}>
+            <div className={`w-full lg:w-[50%]`}>
               <div className="mt-12">
                 <GoogleMapComponent />
               </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
           <div className='flex flex-col items-center justify-center'>
             <FaSlidersH fill='#0041A0' size={35} />
             <h4 className='text-darkText pt-4 font-normal tracking-wide text-center text-[14px]'>Centralized Property Control</h4>
-            <p className='text-textLightGrey text-[12px] text-center'>Manage multiple properties all from one centralized dashboard</p>
+            <p className='text-textLightGrey text-[12px] text-center'>Manage multiple properties all from one <br/> centralized dashboard</p>
           </div>
           <div className='flex flex-col items-center justify-center'>
             <PiCoinsDuotone fill='#0041A0' size={35} />
@@ -42,23 +42,24 @@ export default function Home() {
             <p className='text-textLightGrey text-[12px] text-center'>We provide you the best platform to manage your properties </p>
           </div>
         </div>
-        <div className='w-[80%] mx-auto'>
+        <div className='w-[82%] mx-auto'>
           <div className='flex justify-between'>
             <div className='w-[50%] mt-[8rem]'>
               <p className='px-[.5rem] py-[.7rem] bg-[#ced4f3] bg-opacity[0.8] w-[36%] text-colorPrimary text-[11px] font-normal tracking-wider text-center'>WHY MYPROPSMANAGER?</p>
-              <h4 className='text-darkText pt-4 font-medium tracking-wide text-[32px] leading-[2.7rem]'>Effortless Property Management <br />at Your Fingertips</h4>
+              <h4 className='text-darkText pt-4 font-medium tracking-wide text-[31px] leading-[2rem]'>Effortless Property</h4>
+              <h4 className='text-darkText pt-4 font-medium tracking-wide text-[31px] leading-[1rem]'> Management at Your Fingertips</h4>
               <p className='mt-[1rem] text-textLightGrey text-[.85rem] leading-[1.3rem]'>Simplify the administration and management of your properties with our
                 user-friendly and feature-rich platform. Whether you're a landlord, tenant,
                 or property manager, our app
                 streamlines your property-related tasks, making property management a breeze.</p>
             </div>
-            <img src='/assets/1st.png' className='w-[45%] h-full object-center object-cover' />
+            <img src='/assets/1st.png' className='w-[48%] h-full object-center object-cover' />
           </div>
           <div className='flex justify-between'>
             <img src='/assets/2nd.png' className='w-[42%] h-full object-center object-cover' />
             <div className='w-[50%] mt-[8rem]'>
               <p className='px-[.5rem] py-[.7rem] bg-[#ced4f3] bg-opacity[0.8] w-[36%] text-colorPrimary text-[11px] font-normal tracking-wider text-center'>WHY MYPROPSMANAGER?</p>
-              <h4 className='text-darkText pt-4 font-medium tracking-wide text-[32px] leading-[2.7rem]'>Enhancing Your Rental Experience</h4>
+              <h4 className='text-darkText pt-4 font-medium tracking-wide text-[32px] leading-[2.7rem]'>Enhancing Your <br/>Rental Experience</h4>
               <p className='mt-[1rem] text-textLightGrey text-[.85rem] leading-[1.3rem]'>Discover the advantages of MYPROPSMANAGER.COM, where we offer an
                 efficiently streamlined platform designed to cater to the needs
                 of both landlords and tenants. Our commitment to enhancing your rental experience sets us apart.</p>
@@ -68,7 +69,8 @@ export default function Home() {
           <div className='flex justify-between'>
             <div className='w-[50%] mt-[8rem]'>
               <p className='px-[.5rem] py-[.7rem] bg-[#ced4f3] bg-opacity[0.8] w-[37%] text-colorPrimary text-[11px] font-normal tracking-wider text-center'>WHY MYPROPSMANAGER?</p>
-              <h4 className='text-darkText pt-4 font-medium tracking-wide text-[32px] leading-[2.7rem]'>Effortless Documentation Management</h4>
+              <h4 className='text-darkText pt-4 font-medium tracking-wide text-[32px] leading-[2rem]'>Effortless</h4>
+              <h4 className='text-darkText pt-4 font-medium tracking-wide text-[32px] leading-[1rem]'>Documentation Management</h4>
               <p className='mt-[1rem] text-textLightGrey text-[.85rem] leading-[1.3rem]'>Effortlessly Organize, Conveniently Access, and Securely Manage Your Property Records on Our Platform</p>
             </div>
             <img src='/assets/3rd.png' className='w-[40%] h-full object-center object-cover' />
