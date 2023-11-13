885052ff7aa7b81e30a4b6af317621feaf57a471
Maroprecious <maroumukoro98@gmail.com>
2023-11-13 13:54:00 +0100

    changes

--- public/components/header.tsx
+++ public/components/header.tsx
@@ -1,9 +1,6 @@
-'use-client'
+'use client'
 
 import Link from 'next/link'
-import logo from '../assets/MPM logo.png'
-import Image from 'next/image'
-
 export const Header = () => {
     return (
         <div className='flex justify-between items-center w-full h-24 bg-backgroundColour lg:p-10 p-[1.2rem] border-[1px] border-gray-300'>
@@ -11,6 +8,7 @@ export const Header = () => {
                 <img src='/assets/MPM logo.png' alt='' className='lg:w-[120px] lg:h-[120px] w-[90px] h-[90px] object-contain' />
             </Link>
             <div className='flex items-center'>
+                <Link href={'/login'}>Get started</Link>
                 <p className='text-colorPrimary mr-8 font-normal lg:text-[.9rem] text-[.75rem]'>About Us</p>
                 <Link href={'/contact'} className='text-colorPrimary lg:text-[.9rem] text-[.75rem]'>Contact Us</Link>
             </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 import { poppins } from '../../public/fonts'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
